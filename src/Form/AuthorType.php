0903e669c7b67855ca0835b595edd4d00647f66e
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-05-07 15:06:53 +0700

    fix form for the author entity

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -3,10 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Author;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
