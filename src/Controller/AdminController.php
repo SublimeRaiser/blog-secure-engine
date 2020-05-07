cb5399da03967b3a2c68cb5be5e5be7a77661b4d
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-05-07 15:09:12 +0700

    add admin section

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Entity\BlogPost;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use App\Repository\BlogPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    /** @var SessionInterface */
+    private $session;
+
+    /** @var AuthorRepository */
+    private $authorRepo;
+
+    /** @var BlogPostRepository */
+    private $blogPostRepo;
+
+    /**
+     * AdminController constructor.
+     * @param EntityManagerInterface $em
+     * @param SessionInterface       $session
+     */
+    public function __construct(EntityManagerInterface $em, SessionInterface $session)
+    {
+        $this->em           = $em;
+        $this->session      = $session;
+        $this->authorRepo   = $em->getRepository(Author::class);
+        $this->blogPostRepo = $em->getRepository(BlogPost::class);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/author/new", name="admin_author_new", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     */
+    public function createAuthor(Request $request)
+    {
+        $currentUsername = $this->getUser()->getUsername();
+        if ($this->authorRepo->findByUsername($currentUsername)) {
+            $this->addFlash('danger', 'Unable to create author, author already exists!');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $author = new Author();
+        $author->setUsername($currentUsername);
+
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($author);
+            $this->em->flush();
+
+            $this->session->set('user_is_author', true);
+            $this->addFlash('success', 'Congrats! You are now an author.');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('admin/author_new.html.twig', [
+            'authorForm' => $form->createView(),
+        ]);
+    }
+}
