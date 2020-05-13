3bed905e4088225ea182a62b505b61faf0ce59ac
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-05-14 00:17:54 +0700

    add action and template to create new blog post

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Author;
 use App\Entity\BlogPost;
 use App\Form\AuthorType;
+use App\Form\BlogPostType;
 use App\Repository\AuthorRepository;
 use App\Repository\BlogPostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -82,6 +84,37 @@ class AdminController extends AbstractController
 
         return $this->render('admin/author_new.html.twig', [
             'authorForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/blog-post/new", name="admin_blog_post_new", methods={"GET","POST"})
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function createBlogPost(Request $request)
+    {
+        $currentUsername = $this->getUser()->getUsername();
+        $author          = $this->authorRepo->findByUserName($currentUsername);
+
+        $blogPost = new BlogPost();
+        $blogPost->setAuthor($author);
+
+        $form = $this->createForm(BlogPostType::class, $blogPost);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($blogPost);
+            $this->em->flush();
+
+            $this->addFlash('sucess', 'Congrats! Your post is created!');
+
+            return $this->redirectToRoute('admin_index');
+        }
+
+        return $this->render('admin/blog_post_new.html.twig', [
+            'blogPostForm' => $form->createView(),
         ]);
     }
 }
