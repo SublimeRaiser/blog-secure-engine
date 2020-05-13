9b0ef95660e3762c60d8a4beee0b02545c48bd26
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-05-13 23:47:14 +0700

    add controller for blog posts

---
+++ src/Controller/BlogPostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Entity\BlogPost;
+use App\Repository\AuthorRepository;
+use App\Repository\BlogPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogPostController extends AbstractController
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    /** @var AuthorRepository */
+    private $authorRepo;
+
+    /** @var BlogPostRepository */
+    private $blogPostRepo;
+
+    /**
+     * BlogPostController constructor.
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em           = $em;
+        $this->authorRepo   = $em->getRepository(Author::class);
+        $this->blogPostRepo = $em->getRepository(BlogPost::class);
+    }
+
+    /**
+     * @Route("/", name="blog_post_index")
+     */
+    public function index()
+    {
+        return $this->render('blog_post/index.html.twig', [
+            'blogPosts' => $this->blogPostRepo->findAll(),
+        ]);
+    }
+}
